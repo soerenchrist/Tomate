12a1de17e7145eee9f7cf60e639154a53e364056
Sören Christ <s.christ@mailbox.org>
2023-04-12 08:18:24 +0000

    Add pack and push targets

--- build/Build.cs
+++ build/Build.cs
@@ -1,7 +1,10 @@
 using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MinVer;
 
 [GitHubActions("ci", GitHubActionsImage.UbuntuLatest,
     On = new[] { GitHubActionsTrigger.Push },
+    ImportSecrets = new[] { nameof(NugetApiKey) },
     InvokedTargets = new[] { nameof(Test) })]
 class Build : NukeBuild
 {
@@ -13,11 +16,17 @@ class Build : NukeBuild
 
     public static int Main() => Execute<Build>(x => x.Compile);
 
+    [Parameter][Secret] readonly string NugetApiKey;
+
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    AbsolutePath PackagesDirectory => RootDirectory / "nupkg";
+
     [Solution]
     readonly Solution Solution;
+    [GitVersion]
+    readonly GitVersion GitVersion;
 
     Target Clean => _ => _
         .Before(Restore)
@@ -54,6 +63,33 @@ class Build : NukeBuild
                 .SetConfiguration(Configuration)
                 .EnableNoBuild()
                 .EnableNoRestore());
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Produces(PackagesDirectory / "*.nupkg")
+        .Executes(() =>
+        {
+            Log.Information("Version: {0}", GitVersion.MajorMinorPatch);
+            DotNetPack(s => s
+                .SetProject(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .SetVersion(GitVersion.MajorMinorPatch));
+        });
+
+    Target Push => _ => _
+        .DependsOn(Pack)
+        .Requires(() => NugetApiKey)
+        .Executes(() =>
+        {
+            GlobFiles(PackagesDirectory, "*.nupkg")
+                .Where(x => !x.EndsWith(".symbols.nupkg"))
+                .ForEach(x => DotNetNuGetPush(s => s
+                    .SetTargetPath(x)
+                    .SetApiKey(NugetApiKey)
+                    .SetSource("https://api.nuget.org/v3/index.json")));
         });
 
 }
